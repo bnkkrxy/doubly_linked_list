bdbfa36de06168fd542e56e11a57bc724ff3fb57
lycopersicum-t <anastasijanovik06@gmail.com>
2026-03-24 13:45:24 +0300

    push_front

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,49 @@
 use std::rc::Rc;
 use std::cell::RefCell;
 use std::rc::Weak;
-
 struct Node<T> {
     value: T,
-    ref_next: Option<Rc<RefCell<Node<T>>>>,
-    ref_prev: Option<Weak<RefCell<Node<T>>>>,
+    next: Option<Rc<RefCell<Node<T>>>>,
+    prev: Option<Weak<RefCell<Node<T>>>>,
 }
 
 pub struct DoublyLinkedList<T> {
-    head: Option<T>,
-    tail: Option<T>,
+    head: Option<Rc<RefCell<Node<T>>>>,
+    tail: Option<Rc<RefCell<Node<T>>>>,
     len: usize,
 }
 
+impl<T> Node<T> {
+    pub fn new(value: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Node { 
+            value, 
+            next: None, 
+            prev: None 
+        }))
+    }
+}
+
 impl<T> DoublyLinkedList<T> {
-    fn push_front() {} //добавление элемента в начало
+
+    fn push_front(&mut self, value: T){  //добавление элемента в начало
+        let new_node = Node::new(value);
+
+        match self.head.take() {
+            Some(ex_head) => {
+                ex_head.borrow_mut().prev = Some(Rc::downgrade(&new_node));
+                new_node.borrow_mut().next = Some(ex_head);
+
+                self.head = Some(new_node);
+            }
+            None => {
+                self.tail = Some(Rc::clone(&new_node));
+                self.head = Some(new_node);
+            }
+        }
+
+        self.len += 1;
+        
+    } 
 
     fn push_back() {} //добавление элемента в конец 
 
@@ -40,5 +68,7 @@ impl<T> DoublyLinkedList<T> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    
 
 }
